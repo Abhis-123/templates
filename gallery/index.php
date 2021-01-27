8f5083035337c5f5bf228799adc40655d60e0cdf
Abhis-123 <64187569+Abhis-123@users.noreply.github.com>
2021-01-27 17:29:45 +0530

    Add files via upload

---
+++ gallery/index.php
@@ -0,0 +1,19 @@
+<?php
+if($_COOKIE['logged-in']=="yes"){
+
+
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make Shop Online</title>
+</head>
+<body>
+    
+</body>
+</html>
